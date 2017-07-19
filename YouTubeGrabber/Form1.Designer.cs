4b552d3674c00699066b64788e4a801cf472621d
Vagios Vlachos <bagios234@gmail.com>
2017-07-19 15:36:33 +0300

    Create layout with textbox and button

--- YouTubeGrabber/Form1.Designer.cs
+++ YouTubeGrabber/Form1.Designer.cs
@@ -28,12 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.mainTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.urlTextBox = new System.Windows.Forms.TextBox();
+            this.downloadUrlButton = new System.Windows.Forms.Button();
+            this.mainTableLayoutPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainTableLayoutPanel
+            // 
+            this.mainTableLayoutPanel.ColumnCount = 2;
+            this.mainTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.mainTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.mainTableLayoutPanel.Controls.Add(this.urlTextBox, 0, 0);
+            this.mainTableLayoutPanel.Controls.Add(this.downloadUrlButton, 1, 0);
+            this.mainTableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainTableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.mainTableLayoutPanel.Name = "mainTableLayoutPanel";
+            this.mainTableLayoutPanel.RowCount = 2;
+            this.mainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.mainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
+            this.mainTableLayoutPanel.Size = new System.Drawing.Size(590, 288);
+            this.mainTableLayoutPanel.TabIndex = 0;
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.urlTextBox.Location = new System.Drawing.Point(3, 3);
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.Size = new System.Drawing.Size(407, 20);
+            this.urlTextBox.TabIndex = 0;
+            // 
+            // downloadUrlButton
+            // 
+            this.downloadUrlButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.downloadUrlButton.Location = new System.Drawing.Point(416, 3);
+            this.downloadUrlButton.Name = "downloadUrlButton";
+            this.downloadUrlButton.Size = new System.Drawing.Size(171, 22);
+            this.downloadUrlButton.TabIndex = 1;
+            this.downloadUrlButton.Text = "Download";
+            this.downloadUrlButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(590, 288);
+            this.Controls.Add(this.mainTableLayoutPanel);
+            this.Name = "Form1";
+            this.Text = "YouTube Grabber";
+            this.mainTableLayoutPanel.ResumeLayout(false);
+            this.mainTableLayoutPanel.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel mainTableLayoutPanel;
+        private System.Windows.Forms.TextBox urlTextBox;
+        private System.Windows.Forms.Button downloadUrlButton;
     }
 }
 
