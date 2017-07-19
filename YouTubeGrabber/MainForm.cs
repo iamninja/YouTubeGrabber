e4ca686cf13cc1ea688f865d6598bb1e9f7eadfb
Vagios Vlachos <bagios234@gmail.com>
2017-07-19 21:51:13 +0300

    Change main form name. Grab text on button click

--- YouTubeGrabber/MainForm.cs
+++ YouTubeGrabber/MainForm.cs
@@ -10,11 +10,17 @@ using System.Windows.Forms;
 
 namespace YouTubeGrabber
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void downloadUrlButton_Click(object sender, EventArgs e)
+        {
+            String urlText = urlTextBox.ToString();
+            Console.WriteLine(urlText);
         }
     }
 }
